d42f17503ee01863d63f0c007dfc75ebedddc08f
Srinu-rj <dnsrinu143@gmail.com>
2025-06-10 15:56:38 +0530

    Spring Oauth2.0

--- src/main/java/com/foodapp/springfoodapp/controller/HomePage.java
+++ src/main/java/com/foodapp/springfoodapp/controller/HomePage.java
@@ -1,7 +1,6 @@
 package com.foodapp.springfoodapp.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -15,14 +14,14 @@ public class HomePage {
         return "hemo"; // Returns index.html from templates folder
     }
 
-    @GetMapping("/private")
-    public String privateEndpoint(@AuthenticationPrincipal OAuth2User principal) {
-        return "Private content for: " + principal.getAttributes().get("name");
-    }
-
-    //TODO: Get Oauth user Deatiles
-    @GetMapping("/profile")
-    public Map<String, Object> userProfile(@AuthenticationPrincipal OAuth2User principal) {
-        return principal.getAttributes();
-    }
+//    @GetMapping("/private")
+//    public String privateEndpoint(@AuthenticationPrincipal OAuth2User principal) {
+//        return "Private content for: " + principal.getAttributes().get("name");
+//    }
+//
+//    //TODO: Get Oauth user Deatiles
+//    @GetMapping("/profile")
+//    public Map<String, Object> userProfile(@AuthenticationPrincipal OAuth2User principal) {
+//        return principal.getAttributes();
+//    }
 }
--- src/main/java/com/foodapp/springfoodapp/outh2client/OauthClient.java
+++ src/main/java/com/foodapp/springfoodapp/outh2client/OauthClient.java
@@ -1,33 +1,33 @@
-package com.foodapp.springfoodapp.outh2client;//package com.foodapp.springfoodapp.outh2client;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
-@EnableWebSecurity
-public class OauthClient {
-
-
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
-                    .authorizeHttpRequests(auth -> auth
-                            .requestMatchers("/api/test/public").permitAll()
-                            .requestMatchers("/api/test/profile", "/api/test/private").authenticated()
-                            .anyRequest().authenticated()
-                    )
-                    .oauth2Login(Customizer.withDefaults());;
-
-            return http.build();
-        }
-
-}
+//package com.foodapp.springfoodapp.outh2client;//package com.foodapp.springfoodapp.outh2client;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class OauthClient {
+//
+//
+//        @Bean
+//        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//            http
+//                    .authorizeHttpRequests(auth -> auth
+//                            .requestMatchers("/api/test/public").permitAll()
+//                            .requestMatchers("/api/test/profile", "/api/test/private").authenticated()
+//                            .anyRequest().authenticated()
+//                    )
+//                    .oauth2Login(Customizer.withDefaults());;
+//
+//            return http.build();
+//        }
+//
+//}
